26feda57ecf932971c08163a0dbd1e57d56831ea
muenzhang <zhangmuen2011@163.com>
2024-05-26 09:33:34 +0800

    change comments

--- Greedy/Silver/P2_2015-High_Card_Wins/Silver_P2_2015-High_Card_Wins.cpp
+++ Greedy/Silver/P2_2015-High_Card_Wins/Silver_P2_2015-High_Card_Wins.cpp
@@ -1,26 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, Bessie[50002], Elsie[50001];
+int n, Bessie[50001], Elsie[50000];
 
 int main() {
     freopen("highcard.in", "r", stdin);
     freopen("highcard.out", "w", stdout);
     cin >> n; 
-    for (int i = 1; i <= n; i++) 
+    for (int i = 0; i < n; i++) 
         cin >> Elsie[i];  
-    Elsie[n + 1] = n * 2;  // Add a sentinel value to the end of Elsie's cards
-    sort(Elsie + 1, Elsie + 1 + n);  // Sort Elsie's cards
+    Elsie[n] = n * 2; // Add a boundary value to prevent bessie cards from missing statistics
+    sort(Elsie, Elsie + n);  // Sort Elsie's cards
 
     // Calculate the cards Bessie has
-    for (int i = 1, j = 1; i <= n + 1; i++) 
+    for (int i = 0, j = 0; i <= n; i++) 
         for (int k = Elsie[i - 1] + 1; k < Elsie[i]; k++)
             Bessie[j++] = k;  // Fill Bessie's cards with the remaining cards not in Elsie's set
 
-    int l = 1, r = 1;
-    while (r <= n) {
+    int l = 0, r = 0;
+    while (r < n) {
         // Find the first card in Bessie's set that is larger than Elsie's current card
-        while (r < n && Elsie[l] > Bessie[r])
+        while (r < n - 1 && Elsie[l] > Bessie[r])
             r++;
         l++, r++;  // Move to the next round
     }
